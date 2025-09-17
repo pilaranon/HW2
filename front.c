5284d0a464902a72ecde66c0d773bf556fd31b63
pilaranon <140930240+pilaranon@users.noreply.github.com>
2025-09-17 15:05:04 -0700

    Update front.c

--- front.c
+++ front.c
@@ -22,25 +22,6 @@ int lexLen;            /* length of current lexeme */
 int token, nextToken;  /* token codes */
 FILE *in_fp;
 
-/* -------- Character classes -------- */
-#define LETTER   0
-#define DIGIT    1
-#define UNKNOWN 99
-#define EOF_CHAR -1
-
-/* -------- Token codes -------- */
-#define INT_LIT     10
-#define IDENT       11
-#define ADD_OP      21
-#define SUB_OP      22
-#define MULT_OP     23
-#define DIV_OP      24
-#define MOD_OP      27
-#define LEFT_PAREN  25
-#define RIGHT_PAREN 26
-#define EOF_TOKEN  -1
-
-
 /******************************************************************
  * FUNCTION: lookup
  * PURPOSE:  Map special characters (+ - * / % ( )) to token codes.
