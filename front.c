6f73d0701722b6327cde7f4f6710af51375931ca
pilaranon <140930240+pilaranon@users.noreply.github.com>
2025-09-17 14:29:37 -0700

    Update front.c

--- front.c
+++ front.c
@@ -1,113 +1,131 @@
 /******************************************************************
- * PURPOSE: Implementation of the lexical analyzer for arithmetic
- *          expressions, based on Sebesta Ch.4.2, extended with
- *          the MOD (%) operator.
- * IMPLEMENTED BY: Nathan Malaluan                                *
- * COURSE: CS 351                                                 *
- * INSTRUCTOR: Prof. Shaun-inn Wu                                 *
- * DATE: 09/17/2025  
+ * front.c – Lexical Analyzer for Arithmetic Expressions
+ *
+ * PURPOSE:
+ *   Scan an input expression and break it into tokens (identifiers,
+ *   integers, operators, parentheses).  Based on Sebesta Ch.4.2,
+ *   with MOD (%) support.
+ *
+ * IMPLEMENTED BY: Nathan Malaluan
+ * COURSE: CS 351
+ * INSTRUCTOR: Prof. Shaun-inn Wu
+ * DATE: 09/17/2025
  ******************************************************************/
 
 #include <stdio.h>
 #include <ctype.h>
 #include "front.h"
 
-char lexeme[100];
-int  charClass;
-char nextChar;
-int  lexLen;
-int  token;
-int  nextToken;
+/* -------- Global variables -------- */
+int charClass;         /* letter / digit / unknown / EOF */
+char lexeme[100];      /* buffer to hold current lexeme */
+char nextChar;         /* current character from input */
+int lexLen;            /* length of current lexeme */
+int token, nextToken;  /* token codes */
 FILE *in_fp;
 
+/* -------- Character classes -------- */
+#define LETTER   0
+#define DIGIT    1
+#define UNKNOWN 99
+#define EOF_CHAR -1
+
+/* -------- Token codes -------- */
+#define INT_LIT     10
+#define IDENT       11
+#define ADD_OP      21
+#define SUB_OP      22
+#define MULT_OP     23
+#define DIV_OP      24
+#define MOD_OP      27
+#define LEFT_PAREN  25
+#define RIGHT_PAREN 26
+#define EOF_TOKEN  -1
+
 /******************************************************************
- * FUNCTION: addChar
- * PURPOSE:  Append nextChar to lexeme if buffer limit not reached.
- * INPUT:    nextChar (global)
- * OUTPUT:   Updates lexeme[] and lexLen.
+ * lookup – Map single-char operators or parens to token codes.
  ******************************************************************/
-void addChar(void) {
-    if (lexLen < 98) {
-        lexeme[lexLen++] = nextChar;
-        lexeme[lexLen] = '\0';
+int lookup(char ch) {
+    switch (ch) {
+        case '(': addChar(); nextToken = LEFT_PAREN; break;
+        case ')': addChar(); nextToken = RIGHT_PAREN; break;
+        case '+': addChar(); nextToken = ADD_OP; break;
+        case '-': addChar(); nextToken = SUB_OP; break;
+        case '*': addChar(); nextToken = MULT_OP; break;
+        case '/': addChar(); nextToken = DIV_OP; break;
+        case '%': addChar(); nextToken = MOD_OP; break;
+        default : addChar(); nextToken = EOF_TOKEN; break;
     }
+    return nextToken;
 }
 
 /******************************************************************
- * FUNCTION: getChar
- * PURPOSE:  Read next character from input file and classify it.
- * OUTPUT:   Sets nextChar and charClass.
+ * addChar – Append nextChar to lexeme (if buffer not full).
+ ******************************************************************/
+void addChar(void) {
+    if (lexLen <= 98) {
+        lexeme[lexLen++] = nextChar;
+        lexeme[lexLen] = '\0';
+    } else
+        printf("Error: lexeme too long\n");
+}
+
+/******************************************************************
+ * getChar – Read next character and classify it.
  ******************************************************************/
 void getChar(void) {
-    int c = fgetc(in_fp);
-    if (c == EOF) { charClass = EOF_CHAR; nextChar = '\0'; }
-    else {
+    int c = getc(in_fp);
+    if (c != EOF) {
         nextChar = (char)c;
         if (isalpha((unsigned char)nextChar)) charClass = LETTER;
         else if (isdigit((unsigned char)nextChar)) charClass = DIGIT;
         else charClass = UNKNOWN;
+    } else {
+        charClass = EOF_CHAR;
+        nextChar = '\0';
     }
 }
 
+/******************************************************************
+ * getNonBlank – Skip whitespace.
+ ******************************************************************/
 void getNonBlank(void) {
     while (isspace((unsigned char)nextChar))
         getChar();
 }
 
 /******************************************************************
- * FUNCTION: lookup
- * PURPOSE:  Map special characters (+ - * / % () ) to token codes.
- * ALGORITHM: switch-case table; returns token code.
- ******************************************************************/
-int lookup(char ch) {
-    switch (ch) {
-        case '(' : addChar(); nextToken = LEFT_PAREN; break;
-        case ')' : addChar(); nextToken = RIGHT_PAREN; break;
-        case '+' : addChar(); nextToken = ADD_OP; break;
-        case '-' : addChar(); nextToken = SUB_OP; break;
-        case '*' : addChar(); nextToken = MULT_OP; break;
-        case '/' : addChar(); nextToken = DIV_OP; break;
-        case '%' : addChar(); nextToken = MOD_OP; break;
-        default  : addChar(); nextToken = EOF_TOKEN; break;
-    }
-    return nextToken;
-}
-
-/******************************************************************
- * FUNCTION: lex
- * PURPOSE:  Main lexical analyzer; returns next token.
- * ALGORITHM: Skips blanks, distinguishes identifiers, numbers,
- *            or single-char operators, including %.
+ * lex – Main lexical analyzer. Prints token/lexeme each time.
  ******************************************************************/
 int lex(void) {
     lexLen = 0;
-    lexeme[0] = '\0';
     getNonBlank();
 
-    if (charClass == LETTER) {
-        addChar(); getChar();
-        while (charClass == LETTER || charClass == DIGIT) {
+    switch (charClass) {
+        case LETTER: /* identifier */
             addChar(); getChar();
-        }
-        nextToken = IDENT;
-    }
-    else if (charClass == DIGIT) {
-        /* Collect full integer constant */
-        addChar(); getChar();
-        while (charClass == DIGIT) { addChar(); getChar(); }
-        nextToken = INT_LIT;
-    }
-    else if (charClass == UNKNOWN) {
-        lookup(nextChar);
-        getChar();
-    }
-    else if (charClass == EOF_CHAR) {
-        nextToken = EOF_TOKEN;
-        sprintf(lexeme, "EOF");
-    }
+            while (charClass == LETTER || charClass == DIGIT) {
+                addChar(); getChar();
+            }
+            nextToken = IDENT;
+            break;
 
-    /* <<< print like textbook >>> */
+        case DIGIT:  /* integer literal */
+            addChar(); getChar();
+            while (charClass == DIGIT) { addChar(); getChar(); }
+            nextToken = INT_LIT;
+            break;
+
+        case UNKNOWN: /* operator or paren */
+            lookup(nextChar);
+            getChar();
+            break;
+
+        case EOF_CHAR: /* end-of-file */
+            nextToken = EOF_TOKEN;
+            sprintf(lexeme, "EOF");
+            break;
+    }
     printf("Next token is: %d, Next lexeme is %s\n", nextToken, lexeme);
-
     return nextToken;
 }
